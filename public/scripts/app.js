9a27861e258a2a3057b2f9eebe3f54990d88ed31
Suman <sgummalla.work@gmail.com>
2017-10-20 17:11:34 -0700

    10202017511PM

---
+++ public/scripts/app.js
@@ -0,0 +1,6 @@
+var demoDiv = document.getElementById("demo");
+demoDiv.innerHTML = "Hello World!!!";
+
+var changeCaption = function(){
+    document.getElementById("btnDate").innerHTML = Date();
+}
